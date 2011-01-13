4a987cadc9a4cba28ef15adba568354e4ac541f6
kumabook <kumabook@3771cde0-e90d-4210-a911-b14204632cb3>
2011-01-13 18:16:16 +0000

    ToDoList.ajmlを追加，クエリからのajmlを読み込み機能をindex.jsに追加

--- generator/webapps/editor/index.js
+++ generator/webapps/editor/index.js
@@ -1,17 +1,25 @@
 ajweb = { locale:"en"};
-
+dojo.config["useXDomain"] = true;
 dojo.require("ajweb.editor.Editor");
 dojo.addOnLoad(
   function(){
    window.editor = new ajweb.editor.Editor("editor", "generate", "upload");
-/*    dojo.xhrGet({
-		  url: "chat.ajml",
-		  handleAs: "xml",
-		  load: function(data){
-		   var ajml =  ajweb.xml.serialize(data);
-		    editor.openAjml(ajml);
-		  }
-		});*/
+    var url = new String(window.location);
+    var query = new Array();
+    var ajml_url;
+    query = url.split("?");
+    if ( !!query[1] ) {
+      ajml_url = query[1].split("=")[1];
+    }
+    if(ajml_url)
+      dojo.xhrGet({
+		    url: ajml_url,//"chat.ajml",
+		    handleAs: "xml",
+		    load: function(data){
+		      var ajml =  ajweb.xml.serialize(data);
+		      editor.openAjml(ajml);
+		    }
+		  });
 //   editor.newApplication("chat");
     
   });
