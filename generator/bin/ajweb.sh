22ba2aa08002f7932a151208bd4f5761bb9f9ce8
kumabook <kumabook@3771cde0-e90d-4210-a911-b14204632cb3>
2011-06-12 08:46:42 +0000

    remove lib

--- generator/bin/ajweb.sh
+++ generator/bin/ajweb.sh
@@ -4,12 +4,12 @@
 OS=`uname`
 echo $OS
 if [ $OS = "Darwin" ]
-then java -cp "classes:lib/compiler/commons-io-1.4.jar:lib/compiler/tools.jar:lib/servlet/jetty-all-7.0.2.v20100331.jar:lib/servlet/servlet-api-2.5.jar:lib/servlet/derby.jar:lib/servlet/sqlite-jdbc-3.7.2.jar:lib/servlet/commons-fileupdload-1.2.2.jar" ajweb.server.Main $1 $2 $3 $4 $5
+then java -cp "classes:lib/compiler/commons-io-2.0.jar:lib/compiler/tools.jar:lib/servlet/jetty-all-7.0.2.v20100331.jar:lib/servlet/servlet-api-2.5.jar:lib/servlet/sqlite-jdbc-3.7.2.jar:lib/compiler/commons-fileupload-1.2.2.jar" ajweb.server.Main $1 $2 $3 $4 $5
 
 
 else if [ $OS = "Linux" ]
-then java -cp "classes:lib/compiler/commons-io-1.4.jar:lib/compiler/tools.jar:lib/servlet/jetty-all-7.0.2.v20100331.jar:lib/servlet/servlet-api-2.5.jar:lib/servlet/derby.jar:lib/servlet/sqlite-jdbc-3.7.2.jar:lib/servlet/commons-fileupdload-1.2.2.jar" ajweb.server.Main $1 $2 $3 $4 $5
+then java -cp "classes:lib/compiler/commons-io-2.0.jar:lib/compiler/tools.jar:lib/servlet/jetty-all-7.0.2.v20100331.jar:lib/servlet/servlet-api-2.5.jar:lib/servlet/sqlite-jdbc-3.7.2.jar:lib/compiler/commons-fileupload-1.2.2.jar" ajweb.server.Main $1 $2 $3 $4 $5
 
-else java -cp "classes;lib/compiler/commons-io-1.4.jar;lib/compiler/tools.jar;lib/servlet/jetty-all-7.0.2.v20100331.jar;lib/servlet/servlet-api-2.5.jar;lib/servlet/derby.jar;lib/servlet/sqlite-jdbc-3.7.2.jar;lib/servlet/commons-fileupdload-1.2.2.jar" ajweb.server.Main $1 $2 $3 $4 $5
+else java -cp "classes;lib/compiler/commons-io-2.0.jar;lib/compiler/tools.jar;lib/servlet/jetty-all-7.0.2.v20100331.jar;lib/servlet/servlet-api-2.5.jar;lib/servlet/sqlite-jdbc-3.7.2.jar;lib/compiler/commons-fileupload-1.2.2.jar" ajweb.server.Main $1 $2 $3 $4 $5
 fi
 fi
